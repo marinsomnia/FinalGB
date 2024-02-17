47eaa4063e59fe1ef02014a0e2d6dcbdb76d3e22
marinsomnia <118461086+marinsomnia@users.noreply.github.com>
2024-02-17 10:04:56 +0300

    Update T3DisplaySemiboldLabel.swift

---
+++ GB-PROJ/GB-PROJ/CustomTextView/Display/T3DisplaySemiboldLabel.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct T3DisplaySemiboldLabel: View {
+    var textLabel: String
+    var color: Color
+    
+    init(textLabel: String, color: Color = .black) {
+        self.textLabel = textLabel
+        self.color = color
+    }
+    
+    var body: some View {
+        Text(LocalizedStringKey(textLabel))
+            .font(.T3DisplaySemibold())
+            .foregroundColor(color)
+    }
+}
+
+struct T3DisplaySemiboldLabel_Previews: PreviewProvider {
+    static var previews: some View {
+        T3DisplaySemiboldLabel(textLabel: "This is a custom text label.")
+        .padding()
+        .previewLayout(.sizeThatFits)
+    }
+}
