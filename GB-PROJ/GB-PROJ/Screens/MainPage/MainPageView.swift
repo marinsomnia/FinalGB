7fc244420efb7bec5f41e790d13f9afb567639a4
marinsomnia <118461086+marinsomnia@users.noreply.github.com>
2024-02-17 10:22:02 +0300

    Update MainPageView.swift

---
+++ GB-PROJ/GB-PROJ/Screens/MainPage/MainPageView.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+
+struct MainPageView: View {
+    @StateObject var viewModel: FilmViewModel
+    @State private var isSecondViewActive = false
+    @State private var isStyled = true
+    @State var selectedMovie: ViewFilm? = nil
+    let columns = [
+        GridItem(.flexible(), spacing: 16), 
+        GridItem(.flexible(), spacing: 16)
+    ]
+    
+    var body: some View {
+            VStack {
+                CustomHeaderView(searchFunc: viewModel.searchByName, textLabel: TabBarProperties.moviesCatalog.tabBarTitle)
+                    .padding(.horizontal, 16)
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 16) {
+                        ForEach(viewModel.films, id: \.self) { movie in
+                            LazyVStack(alignment: .leading){
+                                Button() {
+                                    selectedMovie = movie
+                                    isSecondViewActive = true
+                                } label: {
+                                    VStack(alignment: .leading) {
+                                        ZStack{
+                                            Image(uiImage: movie.poster ?? UIImage())
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .cornerRadius(10)
+                                        }
+                                        .padding([.leading, .trailing], 5)
+                                        
+                                        Text(movie.name)
+                                            .font(.system(size: 16))
+                                            .lineLimit(1)
+                                    }
+                                }
+                                .sheet(isPresented: $isSecondViewActive) {
+                                    DescriptionMovieView(viewFilm: selectedMovie ?? viewModel.films[0])
+                                }
+                            }
+                            .foregroundColor(Color.white)
+                            .padding()
+                            .cornerRadius(10)
+                            .frame(height: 250)
+                        }
+                    }
+                    .padding(16)
+                }
+            }
+            .background(Color.Neutral.num3)
+            .onAppear {
+                viewModel.loadData()
+            }
+    }
+    struct NoButtonStyleModifier: ViewModifier {
+        func body(content: Content) -> some View {
+            content
+        }
+    }
+}
+
+//struct MainPageView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MainPageView()
+//    }
+//}
