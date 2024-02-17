178ac8f05cf22aa96b04c1c36f9278e5c6e3683d
marinsomnia <118461086+marinsomnia@users.noreply.github.com>
2024-02-17 10:03:55 +0300

    Update CustomDisplayTextLabel.swift

---
+++ GB-PROJ/GB-PROJ/CustomTextView/Display/CustomDisplayTextLabel.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct CustomDisplayTextLabel: View {
+    var textLabel: String
+    var color: Color
+    var style: TextStyleDisplayProperties
+    
+    init(textLabel: String, color: Color = .black, style: TextStyleDisplayProperties) {
+        self.textLabel = textLabel
+        self.color = color
+        self.style = style
+    }
+    
+    var body: some View {
+        Text(LocalizedStringKey(textLabel))
+            .font(style.font)
+            .foregroundColor(color)
+    }
+}
+
+struct CustomDisplayTextLabel_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            CustomDisplayTextLabel(textLabel: "This is a custom text label.", style: .T1DisplaySemibold)
+            CustomDisplayTextLabel(textLabel: "This is a custom text label.", style: .T2DisplayRegular)
+//            CustomDisplayTextLabel(textLabel: "This is a custom text label.", style: .T3DisplaySemibold)
+//            CustomDisplayTextLabel(textLabel: "This is a custom text label.", style: .T4DisplayRegular)
+        }
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
