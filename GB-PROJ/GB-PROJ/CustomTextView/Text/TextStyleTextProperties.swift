b478ec6d11b01f203b9abc12f4032f3a934901a2
marinsomnia <118461086+marinsomnia@users.noreply.github.com>
2024-02-17 10:06:21 +0300

    Update TextStyleTextProperties.swift

---
+++ GB-PROJ/GB-PROJ/CustomTextView/Text/TextStyleTextProperties.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+enum TextStyleTextProperties {
+    case T1TextRegular
+    case T2TextSemibold
+    case T3TextSemibold
+    case T4TextRegular
+    
+    var font: Font {
+        switch self {
+        case .T1TextRegular:
+            return .T1TextRegular()
+        case .T2TextSemibold:
+            return .T2TextSemibold()
+        case .T3TextSemibold:
+            return .T3TextSemibold()
+        case .T4TextRegular:
+            return .T4TextRegular()
+        }
+    }
+}
