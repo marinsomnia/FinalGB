b2d20e5e7266d60e3b52ca2b28d0dfd8401aa153
marinsomnia <118461086+marinsomnia@users.noreply.github.com>
2024-02-17 10:16:52 +0300

    Update ViewFilm.swift

---
+++ GB-PROJ/GB-PROJ/Models/ViewFilm.swift
@@ -0,0 +1,64 @@
+import Foundation
+import SwiftUI
+import Combine
+import CoreData
+
+struct ViewFilmViewModel {
+    let name: String
+    let poster: UIImage?
+}
+
+class ViewFilm: MediaItem,  Identifiable, Hashable, ObservableObject {
+    
+    let id: Int?
+    let kpRating: Double
+    let imdbEating: Double
+    let criticsRating: Double
+    let name: String
+    let description: String
+    let year: Int
+    let genres: String
+    let countries: String
+    let movieLength: Int
+    let poster: UIImage?
+        
+    init(id: Int?,
+         kpRating: Double?,
+         imdbEating: Double?,
+         criticsRating: Double?,
+         name: String?,
+         description: String?,
+         year: Int?,
+         poster: UIImage,
+         countries: [Country],
+         genres: [Genre],
+         movieLength: Int?)
+    {
+        self.id = id ?? 0
+        self.kpRating = kpRating ?? 0.0
+        self.imdbEating = imdbEating ?? 0.0
+        self.criticsRating = criticsRating ?? 0.0
+        self.name = name ?? "Неизвестно"
+        self.description = description ?? "Без описания"
+        self.year = year ?? 0
+        self.genres = genres.map { $0.name ?? "Неизвестно" }.joined(separator: ", ")
+        self.countries = countries.map { $0.name ?? "Неизвестно" }.joined(separator: ", ")
+        self.movieLength = movieLength ?? 0
+        self.poster = poster
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    // Также можно переопределить оператор == для сравнения объектов
+    static func == (lhs: ViewFilm, rhs: ViewFilm) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
+extension ViewFilm {
+    static func placeholder() -> ViewFilm {
+        return ViewFilm(id: 1, kpRating: 5.0, imdbEating: 5.0, criticsRating: 5.0, name: "Заглушка для фильма", description: "", year: 2000, poster: UIImage(named: "movieTest") ?? UIImage(), countries: [], genres: [], movieLength: 129)
+    }
+}
