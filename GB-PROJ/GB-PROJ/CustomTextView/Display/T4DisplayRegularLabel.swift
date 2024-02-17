a3f306512fd75a6d531b996b72bd0e40ae4503f9
marinsomnia <118461086+marinsomnia@users.noreply.github.com>
2024-02-17 10:05:13 +0300

    Update T4DisplayRegularLabel.swift

---
+++ GB-PROJ/GB-PROJ/CustomTextView/Display/T4DisplayRegularLabel.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct T4DisplayRegularLabel: View {
+    var textLabel: String
+    var color: Color
+    
+    init(textLabel: String, color: Color = .black) {
+        self.textLabel = textLabel
+        self.color = color
+    }
+    
+    var body: some View {
+        Text(LocalizedStringKey(textLabel))
+            .font(.T4DisplayRegular())
+            .foregroundColor(color)
+    }
+}
+
+struct T4DisplayRegularLabel_Previews: PreviewProvider {
+    static var previews: some View {
+        T4DisplayRegularLabel(textLabel: "This is a custom text label.")
+        .padding()
+        .previewLayout(.sizeThatFits)
+    }
+}
